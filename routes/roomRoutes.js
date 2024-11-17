673046b8fc4983e07c9e19b1a8af9ae04fd39ceb
HamdaniDhiaeddine <hamdanidhia4@gmail.com>
2024-11-17 12:05:12 +0100

    invite users to a private room

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { createRoom } = require('../controllers/roomController');
+const { createRoom, inviteUsers } = require('../controllers/roomController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 // Create Room API
 router.post('/create-room', verifyToken, createRoom);
+
+// Invite Users API
+router.post('/:roomId/invite', verifyToken, inviteUsers);
 
 module.exports = router;
